11dc6259f628d1256f5b097ab1a86e842893305f
dariiagrim <grimalskayad@gmail.com>
2022-05-14 13:50:52 +0300

    vector tests

--- src/vector.rs
+++ src/vector.rs
@@ -116,3 +116,178 @@ impl Neg for Vector {
         Vector::new(-self.x, -self.y, -self.z)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let vector = Vector::new(5., 4., 3.);
+        assert_eq!(5., vector.x);
+        assert_eq!(4., vector.y);
+        assert_eq!(3., vector.z);
+    }   
+
+    #[test]
+    fn test_dot() {
+        let vector = Vector::new(5., 4., 3.);
+        let v1 = Vector::new(1., 2., 3.);
+        let v2 = Vector::new(-5., 4., 7.);
+        let v3 = Vector::new(1., 1., 1.);
+        let result1 = vector.dot(v1);
+        let result2 = vector.dot(v2);
+        let result3 = vector.dot(v3);
+        assert_eq!(22., result1);
+        assert_eq!(12., result2);
+        assert_eq!(12., result3);
+    }   
+
+    #[test]
+    fn test_cross() {
+        let vector = Vector::new(5., 4., 3.);
+        let v1 = Vector::new(1., 2., 3.);
+        let v2 = Vector::new(-5., 4., 7.);
+        let v3 = Vector::new(1., 1., 1.);
+        let result1 = vector.cross(v1);
+        let result2 = vector.cross(v2);
+        let result3 = vector.cross(v3);
+        assert_eq!(Vector::new(6., -12., 6.), result1);
+        assert_eq!(Vector::new(16., -50., 40.), result2);
+        assert_eq!(Vector::new(1., -2., 1.), result3);
+    }   
+
+
+
+    #[test]
+    fn test_len() {
+        let v1 = Vector::new(5., 0., 0.);
+        let v2 = Vector::new(-5., 4., 7.);
+        let v3 = Vector::new(1., 1., 1.);
+        let result1 = v1.len();
+        let result2 = v2.len();
+        let result3 = v3.len();
+        assert_eq!(5., result1);
+        assert_eq!((90f64).sqrt(), result2);
+        assert_eq!((3f64).sqrt(), result3);
+    }   
+
+    #[test]
+    fn test_len_sq() {
+        let v1 = Vector::new(5., 0., 0.);
+        let v2 = Vector::new(-5., 4., 7.);
+        let v3 = Vector::new(1., 1., 1.);
+        let result1 = v1.len_sq();
+        let result2 = v2.len_sq();
+        let result3 = v3.len_sq();
+        assert_eq!(25., result1);
+        assert_eq!(90., result2);
+        assert_eq!(3., result3);
+    }  
+
+    #[test]
+    fn test_normailize() {
+        let v1 = Vector::new(5., 0., 0.);
+        let v2 = Vector::new(4., 0., 3.);
+        let result1 = v1.normalize();
+        let result2 = v2.normalize();
+        assert_eq!(Vector::new(1., 0., 0.), result1);
+        assert_eq!(Vector::new(0.8, 0., 0.6), result2);
+      
+    }   
+
+    #[test]
+    fn test_from() {
+        let a = Vector::from((1., 2., 3.));
+        assert_eq!(1., a.x);
+        assert_eq!(2., a.y);
+        assert_eq!(3., a.z);
+        let b: Vector = (2., 3., 5.).into();
+        assert_eq!(2., b.x);
+        assert_eq!(3., b.y);
+        assert_eq!(5., b.z);
+    }
+
+    #[test]
+    fn test_add() {
+        let vector = Vector::new(5., 4., 3.);
+        let v1 = Vector::new(1., 2., 3.);
+        let v2 = Vector::new(-5., 4., 7.);
+        let v3 = Vector::new(1., 1., 1.);
+        let result1 = vector + v1;
+        let result2 = vector + v2;
+        let result3 = vector + v3;
+        assert_eq!(Vector::new(6., 6., 6.), result1);
+        assert_eq!(Vector::new(0., 8., 10.), result2);
+        assert_eq!(Vector::new(6., 5., 4.), result3);
+    }   
+
+
+    #[test]
+    fn test_add_assign() {
+        let mut vector = Vector::new(5., 4., 3.);
+        let v1 = Vector::new(1., 2., 3.);
+        vector += v1;
+        assert_eq!(Vector::new(6., 6., 6.), vector);
+    }  
+
+    #[test]
+    fn test_sub() {
+        let vector = Vector::new(5., 4., 3.);
+        let v1 = Vector::new(1., 2., 3.);
+        let v2 = Vector::new(-5., 4., 7.);
+        let v3 = Vector::new(1., 1., 1.);
+        let result1 = vector - v1;
+        let result2 = vector - v2;
+        let result3 = vector - v3;
+        assert_eq!(Vector::new(4., 2., 0.), result1);
+        assert_eq!(Vector::new(10., 0., -4.), result2);
+        assert_eq!(Vector::new(4., 3., 2.), result3);
+    }   
+
+
+    #[test]
+    fn test_sub_assign() {
+        let mut vector = Vector::new(5., 4., 3.);
+        let v1 = Vector::new(1., 2., 3.);
+        vector -= v1;
+        assert_eq!(Vector::new(4., 2., 0.), vector);
+    }  
+
+    #[test]
+    fn test_mul() {
+        let vector = Vector::new(5., 4., 3.);
+        let result1 = vector * 2.;
+        let result2 = 2. * vector;
+        assert_eq!(Vector::new(10., 8., 6.), result1);
+        assert_eq!(Vector::new(10., 8., 6.), result2);
+    }  
+    
+    #[test]
+    fn test_mul_assign() {
+        let mut vector = Vector::new(5., 4., 3.);
+        vector *= 2.;
+        assert_eq!(Vector::new(10., 8., 6.), vector);
+    }  
+
+    #[test]
+    fn test_dic() {
+        let vector = Vector::new(5., 4., 3.);
+        let result1 = vector / 2.;
+        assert_eq!(Vector::new(2.5, 2., 1.5), result1);
+    }  
+    
+    #[test]
+    fn test_div_assign() {
+        let mut vector = Vector::new(5., 4., 3.);
+        vector /= 2.;
+        assert_eq!(Vector::new(2.5, 2., 1.5), vector);
+    }  
+
+    #[test]
+    fn test_neg() {
+        let mut vector = Vector::new(5., 4., 3.);
+        vector = -vector;
+        assert_eq!(Vector::new(-5., -4., -3.), vector);
+    }  
+}
