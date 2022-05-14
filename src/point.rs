9e27501be66c7b0408ade2d371cb7fd3831caeaa
dariiagrim <grimalskayad@gmail.com>
2022-05-14 12:49:07 +0300

    point tests

--- src/point.rs
+++ src/point.rs
@@ -43,3 +43,58 @@ impl Sub<Vector> for Point {
         Point::new(self.x - rhs.x, self.y - rhs.y, self.z - rhs.z)
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let point = Point::new(5., 4., 3.);
+        assert_eq!(5., point.x);
+        assert_eq!(4., point.y);
+        assert_eq!(3., point.z);
+    }
+
+    #[test]
+    fn test_from() {
+        let a = Point::from((1., 2., 3.));
+        assert_eq!(1., a.x);
+        assert_eq!(2., a.y);
+        assert_eq!(3., a.z);
+        let b: Point = (2., 3., 5.).into();
+        assert_eq!(2., b.x);
+        assert_eq!(3., b.y);
+        assert_eq!(5., b.z);
+    }
+    #[test]
+    fn test_add_vector() {
+        let point = Point::new(5., 4., 3.);
+        let vector = Vector::new(3., 2., 8.);
+        let result = point + vector;
+        assert_eq!(8., result.x);
+        assert_eq!(6., result.y);
+        assert_eq!(11., result.z);
+    }
+
+    #[test]
+    fn test_sub_point() {
+        let point1 = Point::new(5., 4., 3.);
+        let point2  = Point::new(3., 2., 8.);
+        let result = point1 - point2;
+        assert_eq!(2., result.x);
+        assert_eq!(2., result.y);
+        assert_eq!(-5., result.z);
+    }
+
+    #[test]
+    fn test_sub_vector() {
+        let point = Point::new(5., 4., 3.);
+        let vector = Vector::new(3., 2., 8.);
+        let result = point - vector;
+        assert_eq!(2., result.x);
+        assert_eq!(2., result.y);
+        assert_eq!(-5., result.z);
+    }
+}
